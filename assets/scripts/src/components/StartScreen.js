edcc05f5fc8db7da88f0bf02f60ea6f0c3fed7a0
Josh Johnson <josh@jshjohnson.com>
2016-03-05 14:21:18 +0000

    Loading html

--- assets/scripts/src/components/StartScreen.js
+++ assets/scripts/src/components/StartScreen.js
@@ -7,23 +7,21 @@ class StartScreen extends Component {
         let hasPreviousScore = previousScore ? true : false;
         return (
             <screen className="fade-in">
-                <div className="window__area">
-                    <div className="window__outer">
-                        <div className="window__inner">
-                            <h1 className="window__focus window__focus--sm">Number Rush</h1>
-                            <h2>Aim of the game</h2>
-                            <p>Translate as many numbers as possible into German before the timer runs out!</p>
-                            <ul className="list-inline">
-                                {(hasPersonalBest) && (
-                                    <li><h3><strong>Personal best:</strong> { personalBest }</h3></li>
-                                )}
-                                {(hasPreviousScore) && (
-                                    <li><h3><strong>Previous score:</strong> { previousScore }</h3></li>
-                                )}
-                            </ul>
-                            <button className="bubble bubble--input window__form-control window__form-control--push" onClick={ startGame }>Start</button>
-                            <p><small>A thing by <a href="https://github.com/jshjohnson/NumberRush">Josh Johnson</a></small></p>
-                        </div>
+                <div className="window__outer">
+                    <div className="window__inner">
+                        <h1 className="window__focus window__focus--sm">Number Rush</h1>
+                        <h2>Aim of the game</h2>
+                        <p>Translate as many numbers as possible into German before the timer runs out!</p>
+                        <ul className="list-inline">
+                            {(hasPersonalBest) && (
+                                <li><h3><strong>Personal best:</strong> { personalBest }</h3></li>
+                            )}
+                            {(hasPreviousScore) && (
+                                <li><h3><strong>Previous score:</strong> { previousScore }</h3></li>
+                            )}
+                        </ul>
+                        <button className="bubble bubble--input window__form-control window__form-control--push" onClick={ startGame }>Start</button>
+                        <p><small>A thing by <a href="https://github.com/jshjohnson/NumberRush">Josh Johnson</a></small></p>
                     </div>
                 </div>
             </screen>
