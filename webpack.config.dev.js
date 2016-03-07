2e0f09b8f66c9ce042fb7ff7f43fe2c1a0ad0eec
Josh Johnson <josh@jshjohnson.com>
2016-03-07 21:44:06 +0000

    Rudimentary test setup

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -15,6 +15,7 @@ module.exports = {
     },
     plugins: [
         new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin(),
         new webpack.DefinePlugin({
             'process.env': {
                 'NODE_ENV': JSON.stringify('development')
